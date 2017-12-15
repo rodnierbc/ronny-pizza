c7c2e4a38210d37c3c0fc806774c5fbb309c5d45
Rodnier Borrego <rodnierbc89@gmail.com>
2017-12-15 10:57:45 -0800

    creating a prototype for the pizza object that allows me to calculate the final price

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,33 @@
 const SIZE_SMALL = 'Small(10")';
 const SIZE_MEDIUM = 'Medium(12")';
 const SIZE_LARGE = 'Large(14")';
+
+const SIZE_CHEESE_NORMAL = 'Normal';
+const SIZE_CHEESE_EXTRA = 'Extra';
+const SIZE__CHEESE_DOBLE = 'Doble';
+const NO_CHEESE = 'No Cheese';
+
 const PIZZA_MY_OWN_TYPE = 'My Pizza Type'
 
+function Customer(customerFirstName, customerLastName, customerEmail, customerPhone, customerAddres, customerState, customerCity, customerZipCode){
+  this.customerFirstName = customerFirstName;
+  this.customerLastName = customerLastName;
+  this.customerEmail = customerEmail;
+  this.customerPhone = customerPhone;
+  this.customerAddres = customerAddres;
+  this.customerState = customerState;
+  this.customerCity = customerCity;
+  this.customerZipCode = customerZipCode;
+}
+
+Customer.prototype.fullName = function () {
+  return this.customerFirstName+" "+this.customerLastName;
+};
 
 function Pizza(pizzaType, pizzaSize, pizzaCheeseSize, pizzaRegularPrice, pizzaId){
   this.pizzaType = pizzaType;
   this.pizzaSize = pizzaSize;
+  this.pizzaCheeseSize = pizzaCheeseSize;
   this.pizzaToppings = [];
   this.pizzaSauces = [];
   this.pizzaRegularPrice = pizzaRegularPrice;
@@ -17,6 +38,25 @@ Pizza.prototype.finnalPrice = function () {
   var finnalPrice = this.pizzaRegularPrice;
   var oneTopping = 2;
   var oneSauce = 1;
-  var pizzaCheeseSizeNormal = 1;
-
+  for (var i = 0; i< this.pizzaToppings.length; i++){
+    finnalPrice += oneTopping;
+  }
+  for (var i = 0; i< this.pizzaSauces.length; i++){
+    finnalPrice += oneSauce;
+  }
+  switch (this.pizzaCheeseSize) {
+    case SIZE_CHEESE_NORMAL:
+      finnalPrice += 1;
+      break;
+    case SIZE_CHEESE_EXTRA:
+      finnalPrice += 2;
+      break;
+    case SIZE__CHEESE_DOBLE:
+      finnalPrice += 3;
+      break;
+    case NO_CHEESE:
+      finnalPrice -= 1;
+      break;
+  }
+  return finnalPrice;
 };
